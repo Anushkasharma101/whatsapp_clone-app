7276a726901b6068636ea6543c9dc3dfe942342a
Anushka Sharma <as0846403@gmail.com>
2023-02-28 20:47:43 +0530

    first commit

---
+++ lib/common/widgets/custom_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  const CustomButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.black,
+        ),
+      ),
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.green,
+        minimumSize: const Size(double.infinity, 50),
+      ),
+    );
+  }
+}
---
+++ lib/config/agora_config.dart
@@ -0,0 +1,5 @@
+class AgoraConfig {
+  static String token = '';
+  static String appId = '2eaa643cbd6949d2bb118aa8475387a0';
+  static String appcertificate = 'f5c47b6e0821452cb1799b09147b88f1';
+}
